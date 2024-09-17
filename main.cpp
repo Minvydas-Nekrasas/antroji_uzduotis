ee7252f8f2c3042c5f598c96bfac4bb96a270344
Minvydas-Nekrasas <minvydas.nekrasas@gmail.com>
2024-09-17 21:18:47 +0300

    feat(struct): pridėta 'Studentas' struktūra mokinio duomenims
    Pridėta 'Studentas' struktūra su šiais laukais:
    - `vardas`: mokinio vardas
    - `pavarde`: mokinio pavardė
    - `nd`: tarpiniai namų darbų rezultatai
    - `egz`: egamino rezultatas
    
    taip pat pridėta `main()` funkcija, kuri išspausdina "Hello World".

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+struct Studentas{
+    string vardas;
+    string pavarde;
+    int nd;
+    int egz;
+};
+
+int main(){
+    cout << "Hello World" << endl;
+}
