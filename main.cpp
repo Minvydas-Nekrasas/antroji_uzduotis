754fcd3e0fad72b0bb257fd75f87c15453243d58
Minvydas-Nekrasas <minvydas.nekrasas@gmail.com>
2024-09-18 15:57:31 +0300

    feat(įrašymas): pridėjau studento informacijos įrašymo funkcionalumą.
    Įrašyti  galima:
    - Studento vardą ir pavardę
    - Namų darbų skaičius
    -Namų darbų pažymius vieną po kito
    - Egzamino rezultatą

--- main.cpp
+++ main.cpp
@@ -1,14 +1,35 @@
 #include <iostream>
-using namespace std;
+#include <vector>
 #include <string>
+
+using namespace std;
 
 struct Studentas{
     string vardas;
     string pavarde;
-    int nd;
+    vector<int> nd;
     int egz;
 };
 
 int main(){
-    cout << "Hello World" << endl;
+    Studentas x;
+    int darbai, pazymys;
+
+    //Prašom duomenų ir juos įrašom į struktūrą
+    cout << "Įrašyk studento vardą: ";
+    getline(cin, x.vardas);
+
+    cout << "įrašyk studento pavardę: ";
+    getline(cin, x.pavarde);
+
+    cout << "Kiek namų darbų? ";
+    cin >> darbai;
+
+    for (int i = 0; i < darbai; ++i) {
+        cout << "Įrašyk namų darbo pažymį " << i + 1 << ": ";
+        cin >> pazymys;
+        x.nd.push_back(pazymys);  // Įdedam į vektorių
+    }
+    cout << "Įrašyk egzamino rezultatą: ";
+    cin >> x.egz;
 }
