0262e3eb4ecc55c3a4a42fbb817c99ccc68e1623
Minvydas-Nekrasas <minvydas.nekrasas@gmail.com>
2024-10-04 09:32:44 +0300

    fix(pagreitinimas): rezultato išspausdinimo funkcijos pagreitinimas.

--- rezultatas.cpp
+++ rezultatas.cpp
@@ -1,36 +1,39 @@
 #include "rezultatas.h"
 #include <iostream>
 #include <iomanip>
+#include <sstream>
 
 // Atspausdinam studentų duomenis
-void spausdinti(const vector<Studentas>& studentai, bool choice){
+void spausdinti(const vector<Studentas>& studentai, bool choice) {
     string pasirinkimas_pav;
     double pasirinkimas;
 
-    if (choice == 0){
+    if (choice == 0) {
         pasirinkimas_pav = "(Vid.)";
-    }
-    else{
+    } else {
         pasirinkimas_pav = "(Med.)";
     }
-    
-    cout << "\n----------------------------------------------\n";
-    cout << left << setw(15) << "Pavardė" //su setw nustatom tarpus
-         << setw(15) << "Vardas" 
-         << "Galutinis " 
-         << pasirinkimas_pav << endl;
-    cout << "----------------------------------------------\n";
+
+    stringstream buffer;  // naudojam stringstream
+
+    buffer << "\n----------------------------------------------\n";
+    buffer << left << setw(15) << "Pavardė"
+           << setw(15) << "Vardas"
+           << "Galutinis "
+           << pasirinkimas_pav << endl;
+    buffer << "----------------------------------------------\n";
 
     for (const auto& studentas : studentai) {
-        if (choice == 0){
+        if (choice == 0) {
             pasirinkimas = vidurkis(studentas.nd, studentas.egz);
-        }
-        else{
+        } else {
             pasirinkimas = mediana(studentas.nd, studentas.egz);
         }
-        cout << left << setw(15) << studentas.pavarde
-             << setw(15) << studentas.vardas
-             << setw(20) << fixed << setprecision(2) << pasirinkimas // setprecision(2) => du skaičiai po kablelio
-             << endl;
+        buffer << left << setw(15) << studentas.pavarde
+               << setw(15) << studentas.vardas
+               << setw(20) << fixed << setprecision(2) << pasirinkimas
+               << endl;
     }
+
+    cout << buffer.str() << flush;  // visk1 atspausdinam vienu metu
 }
