6a6a2e529b24f7c2fa65b424fed6e205aa036104
Minvydas-Nekrasas <minvydas.nekrasas@gmail.com>
2024-11-22 16:51:36 +0200

    feat(class): struct pakeista į class

--- 3_strategija/List/studentas.h
+++ 3_strategija/List/studentas.h
@@ -3,16 +3,55 @@
 
 #include <string>
 #include <vector>
+#include <numeric>
 
 using namespace std;
 
-
-struct Studentas {
+class Studentas {
+private:
     string vardas;
     string pavarde;
     vector<int> nd;
     int egz;
     double galutinis;
+
+public:
+    // Constructors
+    Studentas() : vardas(""), pavarde(""), egz(0), galutinis(0.0) {}
+    Studentas(const string& v, const string& p, const vector<int>& n, int e)
+        : vardas(v), pavarde(p), nd(n), egz(e) {
+        calculateGalutinis();
+    }
+
+    // Getters and setters
+    string getVardas() const { return vardas; }
+    void setVardas(const string& v) { vardas = v; }
+
+    string getPavarde() const { return pavarde; }
+    void setPavarde(const string& p) { pavarde = p; }
+
+    vector<int> getNd() const { return nd; }
+    void setNd(const vector<int>& n) { nd = n; calculateGalutinis(); }
+
+    int getEgz() const { return egz; }
+    void setEgz(int e) { egz = e; calculateGalutinis(); }
+
+    double getGalutinis() const { return galutinis; }
+
+    // Methods
+    void calculateGalutinis() {
+        if (!nd.empty()) {
+            double nd_avg = accumulate(nd.begin(), nd.end(), 0.0) / nd.size();
+            galutinis = 0.4 * nd_avg + 0.6 * egz;
+        } else {
+            galutinis = 0.6 * egz;
+        }
+    }
+
+    void addNd(int grade) {
+        nd.push_back(grade);
+        calculateGalutinis();
+    }
 };
 
 #endif
