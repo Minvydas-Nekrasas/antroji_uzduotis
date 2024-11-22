75086fe30be425293b871c96d0013a5e0ef9cefd
Minvydas-Nekrasas <minvydas.nekrasas@gmail.com>
2024-11-22 16:53:07 +0200

    feat(rikiavimas getteriai): pakoreguota rikiavimo funkcija

--- 3_strategija/List/rikiavimas.cpp
+++ 3_strategija/List/rikiavimas.cpp
@@ -3,35 +3,31 @@
 
 using namespace std;
 
-
-// Pagalbinė funkcija rikiavimui
-string toLowerCase(const string& str) {
-    string lowerStr = str;
-    transform(lowerStr.begin(), lowerStr.end(), lowerStr.begin(), ::tolower);
-    return lowerStr;
-}
-// Funkcija studentų rikiavimui pagal vardą arba pavardę
 void rikiuotiStudentus(list<Studentas>& studentai, int sort_choice) {
     if (sort_choice == 1) {
         // Rikiuojam pagal vardą
         studentai.sort([](const Studentas& a, const Studentas& b) {
-            if (toLowerCase(a.vardas) == toLowerCase(b.vardas)) {
-                return toLowerCase(a.pavarde) < toLowerCase(b.pavarde);
+            string vardasA = a.getVardas();
+            string vardasB = b.getVardas();
+            if (vardasA == vardasB) {
+                return a.getPavarde() < b.getPavarde();
             }
-            return toLowerCase(a.vardas) < toLowerCase(b.vardas);
+            return vardasA < vardasB;
         });
     } else if (sort_choice == 0) {
         // Rikiuojam pagal pavardę
         studentai.sort([](const Studentas& a, const Studentas& b) {
-            if (toLowerCase(a.pavarde) == toLowerCase(b.pavarde)) {
-                return toLowerCase(a.vardas) < toLowerCase(b.vardas);
+            string pavardeA = a.getPavarde();
+            string pavardeB = b.getPavarde();
+            if (pavardeA == pavardeB) {
+                return a.getVardas() < b.getVardas();
             }
-            return toLowerCase(a.pavarde) < toLowerCase(b.pavarde);
+            return pavardeA < pavardeB;
         });
     } else if (sort_choice == 2) {
         // Rikiuojam pagal galutinį pažymį
         studentai.sort([](const Studentas& a, const Studentas& b) {
-            return a.galutinis > b.galutinis; // Didžiausias pirmas
+            return a.getGalutinis() > b.getGalutinis();
         });
     }
 }
